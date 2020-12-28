98a7be45be9e5c97e026275e2f8db36a69ad8df5
Chalermrid Patipanang <chalermrid.patipanang@tesco.com>
2020-12-28 15:23:00 +0700

    enable cors

--- src/home/hot-offers/hot-offers.controller.ts
+++ src/home/hot-offers/hot-offers.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get } from '@nestjs/common';
 import { HotOffer } from './hot-offers.model';
 import { HotOffersService } from './hot-offers.service';
 
-@Controller('hot-offers')
+@Controller('home/hot-offers')
 export class HotOffersController {
   constructor(private hotOfferService: HotOffersService) {}
 
