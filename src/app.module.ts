20f43ed86daff2793d0855195e474b89bb1e2e4e
Chalermrid Patipanang <chalermrid.patipanang@tesco.com>
2021-01-06 10:48:38 +0700

    add health check endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { HomeModule } from './home/home.module';
 import { CategoriesModule } from './categories/categories.module';
 import { ProductsModule } from './products/products.module';
+import { HealthController } from './health/health.controller';
+import { TerminusModule } from '@nestjs/terminus';
 
 @Module({
-  imports: [HomeModule, CategoriesModule, ProductsModule],
+  imports: [TerminusModule, HomeModule, CategoriesModule, ProductsModule],
+  controllers: [HealthController],
 })
 export class AppModule {}
